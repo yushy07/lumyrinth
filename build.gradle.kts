5be06c9ad298fc322e096250419c2cc00b7c0acc
yushy07 <yushy07@users.noreply.github.com>
2026-08-26 18:16:54 +0530

    fix: use published cryptography artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,15 @@ buildscript {
     configurations.all {
         resolutionStrategy {
             force("org.jdom:jdom2:2.0.6.1")
-            force("org.bouncycastle:bcprov-jdk18on:1.85.2")
-            force("org.bouncycastle:bcpkix-jdk18on:1.85.2")
+            force("org.bouncycastle:bcprov-jdk18on:1.85")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
             force("org.bitbucket.b_c:jose4j:0.9.6")
             force("org.apache.commons:commons-lang3:3.20.0")
             force("org.apache.httpcomponents:httpclient:4.5.14")
             eachDependency {
                 if (requested.group == "io.netty") useVersion("4.1.137.Final")
                 if (requested.group == "org.bouncycastle" && requested.name.endsWith("jdk18on")) {
-                    useVersion("1.85.2")
+                    useVersion("1.85")
                 }
             }
         }
@@ -27,15 +27,15 @@ allprojects {
     configurations.all {
         resolutionStrategy {
             force("org.jdom:jdom2:2.0.6.1")
-            force("org.bouncycastle:bcprov-jdk18on:1.85.2")
-            force("org.bouncycastle:bcpkix-jdk18on:1.85.2")
+            force("org.bouncycastle:bcprov-jdk18on:1.85")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
             force("org.bitbucket.b_c:jose4j:0.9.6")
             force("org.apache.commons:commons-lang3:3.20.0")
             force("org.apache.httpcomponents:httpclient:4.5.14")
             eachDependency {
                 if (requested.group == "io.netty") useVersion("4.1.137.Final")
                 if (requested.group == "org.bouncycastle" && requested.name.endsWith("jdk18on")) {
-                    useVersion("1.85.2")
+                    useVersion("1.85")
                 }
             }
         }
